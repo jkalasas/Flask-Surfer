8eade047c517b27c1804f1763dd41b8421c869e8
jkalasas <alasasjohnkyle@gmail.com>
2022-08-27 23:01:10 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+"""
+Flask-Surfer
+------------
+
+Extension to add CSRF protection to flask applications.
+"""
+
+from setuptools import setup
+
+setup(
+    name="Flask-Surfer",
+    version="0.0.1",
+    license="MIT",
+    author="John Kyle Alas-as",
+    author_email="alasasjohnkyle@gmail.com",
+    url="https://github.com/jkalasas/Flask-Surfer",
+    long_description=__doc__,
+    keywords=[
+        "csrf",
+        "flask",
+        "protection",
+        "security",
+    ],
+    packages=[
+        "flask_surfer",
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    platforms="any",
+    install_requirements=[
+        "flask",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
